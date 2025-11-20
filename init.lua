726a079f34c60781651d600757f3a4660afd53a4
Ynoob87 <hhgg12661@gmail.com>
2025-11-20 21:07:09 +0800

    refactor: remove telescope and treesitter configurations; add core initialization and keymaps
    - Deleted telescope and treesitter plugin configurations.
    - Introduced core initialization file to require options and keymaps.
    - Added keymaps for various functionalities including navigation, editing, and diagnostics.
    - Created options file to set up Vim configurations such as line numbers, indentation, and UI settings.
    - Implemented lazy loading for plugins using lazy.nvim.
    - Added new plugins for auto-pairs, color schemes, file management, and snacks functionality.

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-require("config.options")
-require("config.lazy")
-require("config.autocmds")
-
-_G.keymaps = require("config.keymaps") -- Load keymaps config and store globally for plugins
+require("root.core")
+require("root.lazy")
+require("current-theme")
